30481413b885cd807673aedca66611f9e9414072
Ariane Schang <ariane.schang@mail.mcgill.ca>
2017-02-26 00:18:42 -0500

    regressions done

--- features.py
+++ features.py
@@ -4,15 +4,39 @@ from collections import defaultdict
 from nltk.corpus import stopwords
 import string
 import pickle
+from string import digits
 
-#stopwords = [u'i', u'me', u'my', u'myself', u'we', u'our', u'ours', u'ourselves', u'you', u'your', u'yours', u'yourself', u'yourselves', u'he', u'him', u'his', u'himself', u'she', u'her', u'hers', u'herself', u'it', u'its', u'itself', u'they', u'them', u'their', u'theirs', u'themselves', u'what', u'which', u'who', u'whom', u'this', u'that', u'these', u'those', u'am', u'is', u'are', u'was', u'were', u'be', u'been', u'being', u'have', u'has', u'had', u'having', u'do', u'does', u'did', u'doing', u'a', u'an', u'the', u'and', u'but', u'if', u'or', u'because', u'as', u'until', u'while', u'of', u'at', u'by', u'for', u'with', u'about', u'against', u'between', u'into', u'through', u'during', u'before', u'after', u'above', u'below', u'to', u'from', u'up', u'down', u'in', u'out', u'on', u'off', u'over', u'under', u'again', u'further', u'then', u'once', u'here', u'there', u'when', u'where', u'why', u'how', u'all', u'any', u'both', u'each', u'few', u'more', u'most', u'other', u'some', u'such', u'no', u'nor', u'not', u'own', u'same', u'so', u'than', u'too', u'very', u's', u't', u'can', u'will', u'just', u'don', u'should', u'now', u'd', u'll', u'm', u'o', u're', u've', u'y', u'ain', u'aren', u'couldn', u'didn', u'doesn', u'hadn', u'hasn', u'haven', u'isn', u'ma', u'mightn', u'mustn', u'needn', u'shan', u'shouldn', u'wasn', u'weren', u'won', u'wouldn']
-stopwords = ['and', 'the']
+stopwords = [ u'this', u'that', u'these', u'those', u'am', u'is', u'are', u'was', u'were', u'be', u'been', u'being', u'have', u'has', u'had', u'having', u'do', u'does', u'did', u'doing', u'a', u'an', u'the', u'and', u'but', u'if', u'or', u'because', u'as', u'until', u'while', u'of', u'at', u'by', u'for', u'with', u'about', u'against', u'between', u'into', u'through', u'during', u'before', u'after', u'above', u'below', u'to', u'from', u'up', u'down', u'in', u'out', u'on', u'off', u'over', u'under', u'again', u'further', u'then', u'once', u'here', u'there', u'when', u'where', u'why', u'how', u'all', u'any', u'both', u'each', u'few', u'more', u'most', u'other', u'some', u'such', u'no', u'nor', u'not', u'own', u'same', u'so', u'than', u'too', u'very', u's', u't', u'can', u'will', u'just', u'don', u'should', u'now', u'd', u'll', u'm', u'o', u're', u've', u'y', u'ain', u'aren', u'couldn', u'didn', u'doesn', u'hadn', u'hasn', u'haven', u'isn', u'ma', u'mightn', u'mustn', u'needn', u'shan', u'shouldn', u'wasn', u'weren', u'won', u'wouldn']
+#stopwords = ['and', 'the', '']
 weasel_words_list = ['some', 'many', 'source', 'sources']
 print stopwords
 print string.punctuation
-punct = list(string.punctuation)
+punct = list(string.punctuation) + ['"', '``', "''"]
 print punct
 import pickle
+
+
+
+myCueWordFile = open('typed_cue_words.txt', 'r')
+allLines = myCueWordFile.readlines()
+verbCategories = {'Presumed True': [], 'Say': [], 'Belief': [], 'Intend': []}
+presumed = allLines[2]
+say = allLines[6]
+belief = allLines[10]
+intend = allLines[15]
+
+
+def splitTrim(mystring):
+	split = mystring.split(',')
+	returnlist = [(i.translate(None, digits)).strip() for i in split]
+	return returnlist
+
+
+verbCategories['Presumed True'] = splitTrim(presumed)
+verbCategories['Say'] = splitTrim(say)
+verbCategories['Belief'] = splitTrim(belief)
+verbCategories['Intend'] = splitTrim(intend)
+
 
 
 quoteMarkers = ['"', '``', "''"]
@@ -56,7 +80,8 @@ def featureExtract(attribution, article):
 
 	featureDict['weaselWordPresence'] = len(weasel_words) > 0
 	featureDict['weaselWords'] = weasel_words
-	
+
+
 
 
 	if len(source) == 0:
@@ -82,6 +107,15 @@ def featureExtract(attribution, article):
 	featureDict['contentWords'] = contentBOW
 	featureDict['sourceWords'] = sourceBOW
 	featureDict['cueBOW'] = cueBOW
+
+	typeVerb = None
+	if len(cue) == 0:
+		typeVerb = None
+		featureDict['verbType'] = [0,0,0,0]
+	else:
+		cueHead = article._find_head(cue)
+		typeVerb = verbType(cueHead)
+		featureDict['verbType'] = typeVerb
 
 	#for key in featureDict.keys():
 	#	print key, featureDict[key]
@@ -155,8 +189,6 @@ def sourceModifier(sourceHead):
 			elif relation == 'det':
 				dets.append(token['lemma'])
 
-		print children
-
 	return amods, dets
 
 def bagOfWords(content):
@@ -172,8 +204,28 @@ def bagOfWords(content):
 			words.append(word['lemma'])
 
 	filtered_words = [word for word in words if (word.lower() not in stopwords and word.lower() not in punct)]
-
 	return filtered_words
+
+def verbType(cue):
+	presumed = verbCategories['Presumed True']
+	say = verbCategories['Say']
+	belief = verbCategories['Belief']
+	intend = verbCategories['Intend']
+
+	for head in cue:
+		lemma = head['lemma']
+		if lemma in presumed:
+			return [1,0,0,0]
+		elif lemma in say:
+			return [0,1,0,0]
+		elif lemma in belief:
+			return [0,0,1,0]
+		elif lemma in intend:
+			return [0,0,0,1]
+	#verbCategories
+	#['presumed', 'say', 'belief', 'intend']
+	
+	return [0,0,0,0] 
 
 ################### VECTORIZE ###################
 
@@ -190,8 +242,10 @@ def vectorize(dataDict):
 	detsVocab = []
 
 	ids = dataDict.keys()
+	i = 0
 	for attr in ids:
 		features = dataDict[attr]
+
 		contentVocab = contentVocab + features['contentWords']
 		sourceVocab = sourceVocab + features['sourceWords']
 		cueVocab = cueVocab + features['cueBOW']
@@ -200,6 +254,7 @@ def vectorize(dataDict):
 		sourceLemmaVocab = sourceLemmaVocab + features['sourceLemma']
 		amodVocab = amodVocab + features['amods']
 		detsVocab = detsVocab +  features['dets']
+
 
 	contentVocab = sorted(list(set(contentVocab)))
 	sourceVocab = sorted(list(set(sourceVocab)))
@@ -210,29 +265,36 @@ def vectorize(dataDict):
 	amodVocab = sorted(list(set(amodVocab)))
 	detsVocab = sorted(list(set(detsVocab)))
 
+	headers = []
 	for attr in ids:
+		headers = []
 		thisVector = []
 		features = dataDict[attr]
+		headers.append('sourceEntityPresence')
 		if features['sourceEntityPresence'] == True:
 			thisVector.append(1)
 		else:
 			thisVector.append(0)
-
+		
+		headers.append('sourcePlural')
 		if features['sourcePlural'] == True:
 			thisVector.append(1)
 		else:
 			thisVector.append(0)
 
+		headers.append('weaselWord')
 		if features['weaselWordPresence'] == True:
 			thisVector.append(1)
 		else:
 			thisVector.append(0)
 
+		headers.append('amodPresence')
 		if features['amodPresence'] == True:
 			thisVector.append(1)
 		else:
 			thisVector.append(0)
 
+		headers.append('typeQuote')
 		if features['quoteType'] == 'direct':
 			thisVector.append(0)
 		elif features['quoteType'] == 'mixed':
@@ -240,28 +302,50 @@ def vectorize(dataDict):
 		else:
 			thisVector.append(2)
 
+		headers.append('pronounSource')
 		if features['pronounPresence'] == True:
 			thisVector.append(1)
 		else:
 			thisVector.append(0)
 
+		headers.append('detSource')
 		if features['detPresence'] == True:
 			thisVector.append(1)
 		else:
 			thisVector.append(0)
 
+		headers = headers + ['presumedVerb', 'sayVerb', 'beliefVerb', 'intendVerb']
+		thisVector = thisVector + features['verbType']
+
+		headers = headers + [('dets_' + i) for i in detsVocab]
 		thisVector = thisVector + createListVectors(features['dets'], detsVocab)
+		
+		headers = headers + [('amod_' + i) for i in amodVocab]
 		thisVector = thisVector + createListVectors(features['amods'], amodVocab)
+		
+		headers = headers + [('sourceLemmaVocab_' + i) for i in sourceLemmaVocab]		
 		thisVector = thisVector + createListVectors(features['sourceLemma'], sourceLemmaVocab)
+		
+		headers = headers + [('typeEntities_' + i) for i in typeEntitiesVocab]		
 		thisVector = thisVector + createListVectors(features['typeEntities'], typeEntitiesVocab)
+		
+		headers = headers + [('weasel_' + i) for i in weaselWordsVocab]		
 		thisVector = thisVector + createListVectors(features['weaselWords'], weaselWordsVocab)
+		
+		headers = headers + [('cueBOW_' + i) for i in cueVocab]		
 		thisVector = thisVector + createListVectors(features['cueBOW'], cueVocab)
+		
+		headers = headers + [('sourceBOW_' + i) for i in sourceVocab]		
 		thisVector = thisVector + createListVectors(features['sourceWords'], sourceVocab)
+		
+		headers = headers + [('contentBOW_' + i) for i in contentVocab]		
 		thisVector = thisVector + createListVectors(features['contentWords'], contentVocab)
 
 		thisVector = features['label'] + thisVector
+		headers = ['label'] + headers
 		featVector.append(thisVector)
 
+	print headers
 	return featVector
 
 def createListVectors(thisList, compareList):
@@ -276,27 +360,26 @@ def createListVectors(thisList, compareList):
 
 	return zeroes
 
-
-
-
 def execute(data):
 	attr_ids = data.keys()
 	i = 0
 	
-	'''
+	
 	for attr in attr_ids:
 		attribution, article = getAttr(attr)
 		featureDict = featureExtract(attribution, article)
 		label = data[attr]
 		featureDict['label'] = label
 		data[attr] = featureDict
+		i += 1
+
 		
 	
 
 	pickle.dump(data, open('featureDict', 'wb'))
-	'''
 	
-	data = pickle.load(open('featureDict', 'rb'))
+	
+	#data = pickle.load(open('featureDict', 'rb'))
 
 
 
@@ -308,7 +391,6 @@ def execute(data):
 
 def openFile(pairwiseFile):
 	finalData = defaultdict(list)
-
 
 	with open(pairwiseFile, 'r') as f:
 		reader = csv.reader(f)
